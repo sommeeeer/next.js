324a35fa433f0aa9079278e6dc213a900b960a6a
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2024-08-23 15:28:46 +0200

    Effect::Worker in analyzer

--- turbopack/crates/turbopack-ecmascript/src/references/mod.rs
+++ turbopack/crates/turbopack-ecmascript/src/references/mod.rs
@@ -1084,6 +1084,9 @@ pub(crate) async fn analyse_ecmascript_module_internal(
                 )
                 .await?;
             }
+            Effect::Worker { .. } => {
+                // TODO: perform worker replacement
+            }
             Effect::MemberCall {
                 obj,
                 prop,
