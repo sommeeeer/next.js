1e9a1f8602ca83eb4396dd8367f79d816d9a970e
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-27 09:40:39 +0900

    squash:
    transforms_after_split
    
    Adjust types
    
    Adjust types to fix build
    
    let mut transforms_after_split = Vc::cell(vec![]);
    
    Fix build, and move server actions to `append_after_split`
    
    apply_transforms
    
    WIP
    
    commentts
    
    fix build
    
    Enable tests
    
    WrapFuture
    
    Add logging
    
    Support comments
    
    Rename
    
    failsafe_parse()
    
    Remove logging
    
    Do not split twcie
    
    Provide source to eval context
    
    Rename: fragment_transforms
    
    Rename: module_transforms
    
    Enable tree shaking for more code
    
    Add logging
    
    Copy directives
    
    Doc
    
    Revert "Add logging"
    
    This reverts commit 8fd6d8515ffc60097a6beb4a7c29858746a4df2d.
    
    Add a test
    
    Add logging
    
    This reverts commit 05121d46bdd5ea1ef2552f094896acb77b2237b5.
    
    Enable tree shaking for more code
    
    Remove logging
    
    This reverts commit 69ea4bce5d699c7e33466b17a9905bffaf0fe4d5.
    
    Add logging
    
    This reverts commit 0a5fc3b243a57528b29a85774b359c95daed7e4d.
    
    Add an assertion
    
    Do not split multiple time
    
    WIP: EsmBinding
    
    Remove logging
    
    Revert "WIP: EsmBinding"
    
    This reverts commit ec77a0f0a601ce16919d54474370a91b40e2be9d.
    
    Fix export src issue
    
    use constant
    
    lint
    
    Escape `default` in `export { default as foo }`
    
    Match assumptions
    
    export default decl
    
    export.src.is_none()
    
    Allow jsx in tests
    
    fix part = Some + Some issue
    
    [TMP] Merge https://github.com/vercel/next.js/pull/69211
    
    Enable logging
    
    Use parse(Some)
    
    split()
    
    Revert "Enable logging"
    
    This reverts commit 34fb403f9dfb4110177a415b71da4a1e0cdcd8f2.
    
    Enable logging
    
    This reverts commit 59c1e3de190cccad6a8cbafe0002ad52b9eba7b2.
    
    Skip directives
    
    Revert "Enable logging"
    
    This reverts commit 337328da6dbafac6289eee66e589d9747213edb4.
    
    Enable logging
    
    This reverts commit 5ed229d4af66622899d5880be0dc5b3ec61202f8.
    
    is_turbopack_var_export

--- turbopack/crates/turbopack-ecmascript/src/references/mod.rs
+++ turbopack/crates/turbopack-ecmascript/src/references/mod.rs
@@ -125,7 +125,7 @@ use crate::{
     },
     chunk::EcmascriptExports,
     code_gen::{CodeGen, CodeGenerateable, CodeGenerateableWithAsyncModuleInfo, CodeGenerateables},
-    magic_identifier, parse,
+    magic_identifier,
     references::{
         async_module::{AsyncModule, OptionAsyncModule},
         cjs::{CjsRequireAssetReference, CjsRequireCacheAccess, CjsRequireResolveAssetReference},
@@ -135,7 +135,7 @@ use crate::{
         require_context::{RequireContextAssetReference, RequireContextMap},
         type_issue::SpecifiedModuleTypeIssue,
     },
-    tree_shake::{find_turbopack_part_id_in_asserts, part_of_module, split},
+    tree_shake::find_turbopack_part_id_in_asserts,
     utils::{module_value_to_well_known_object, AstPathRange},
     EcmascriptInputTransforms, EcmascriptModuleAsset, EcmascriptParsable, SpecifiedModuleType,
     TreeShakingMode,
@@ -448,13 +448,7 @@ pub(crate) async fn analyse_ecmascript_module_internal(
         EcmascriptModuleAssetType::Ecmascript => false,
     };
 
-    let parsed = if let Some(part) = part {
-        let parsed = parse(source, ty, transforms);
-        let split_data = split(source.ident(), source, parsed);
-        part_of_module(split_data, part)
-    } else {
-        module.failsafe_parse()
-    };
+    let parsed = module.failsafe_parse(part);
 
     let ModuleTypeResult {
         module_type: specified_type,
