e18a24dba491576484ca5a224677849fc6e0ffe8
Jiachi Liu <inbox@huozhi.im>
2024-08-29 22:58:23 +0200

    add test

--- test/development/app-dir/stitching-errors/stitching-errors.test.ts
+++ test/development/app-dir/stitching-errors/stitching-errors.test.ts
@@ -6,8 +6,8 @@ describe('stitching errors', () => {
     files: __dirname,
   })
 
-  it('should log stitched error for browser errors', async () => {
-    const browser = await next.browser('/browser')
+  it('should log stitched error for browser uncaught errors', async () => {
+    const browser = await next.browser('/uncaught')
     const logs = await browser.log()
 
     await retry(() => {
@@ -25,6 +25,32 @@ describe('stitching errors', () => {
             // Extra info of original component
             message: expect.stringContaining(
               'The above error occurred in the <Page> component. It was handled by the <ReactDevOverlay> error boundary.'
+            ),
+          }),
+        ])
+      )
+    })
+  })
+
+  it('should log stitched error for browser caught errors', async () => {
+    const browser = await next.browser('/caught')
+    const logs = await browser.log()
+
+    await retry(() => {
+      expect(logs).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            // Original error stack
+            message: expect.stringContaining('at useThrowError'),
+          }),
+          expect.objectContaining({
+            // Stitched error stack
+            message: expect.stringContaining('at ClientPageRoot'),
+          }),
+          expect.objectContaining({
+            // Extra info of caught boundary
+            message: expect.stringContaining(
+              'The above error occurred in the <Thrower> component. It was handled by the <MyErrorBoundary> error boundary.'
             ),
           }),
         ])
